3b2a5c35c4a45228bede4decb551a798b00225c7
Josef Svenningsson <josef.svenningsson@gmail.com>
2006-12-29 00:22:09 +0100

    Compute bounding boxes around Drawing
    darcs-hash:20061228232209-f316c-21c49e18fa4ae8b320a572aa51cbccb809442cbf

--- Drawing.hs
+++ Drawing.hs
@@ -22,3 +22,34 @@ identityTransform = Transform { dx      = 0,
 				dsize   = 1,
 				dbright = 1,
 				drotate = 0 }
+
+pruneAtDepth :: Int -> Drawing -> Drawing
+pruneAtDepth 0 _ = Branch []
+pruneAtDepth n (Transformation t d) = Transformation t (pruneAtDepth (n-1) d)
+pruneAtDepth n (Branch ds) = Branch (map (pruneAtDepth (n-1)) ds)
+pruneAtDepth n d = d
+
+data BBox = BBox { x :: !Double
+                 , y :: !Double
+                 , w :: !Double
+                 , h :: !Double }
+
+boundingBox :: Drawing -> BBox
+boundingBox (Transformation tr dr) = transformBBox tr (boundingBox dr)
+boundingBox (Branch ds) = foldr1 unionBBox (map boundingBox ds)
+boundingBox _ = BBox -0.5 -0.5 1 1
+
+transformBBox :: Transform -> BBox -> BBox
+transformBBox (Transform dx dy ds _ dr) (BBox x y w h) 
+  = BBox ((cos dr * x + sin dr * y)*ds + dx) 
+         ((sin dr * x + cos dr * y)*ds + dy) 
+         ((cos dr * w + sin dr * h)*ds) 
+         ((sin dr * w + cos dr * h)*ds)
+
+unionBBox :: BBox -> BBox -> BBox
+unionBBox (BBox x1 y1 h1 w1) (BBox x2 y2 h2 w2)
+  = BBox x y w h
+  where x = min x1 x2
+        y = min y1 y2
+        w = max (x1 + w1) (x2 + w2) - x
+        h = max (y1 + h1) (y2 + h2) - y
